bb28282f0c969d3b480a20917c5819088fb98c45
AfafHanbali <53369567+AfafHanbali@users.noreply.github.com>
2020-11-23 15:29:07 +0200

    login page update

--- app/src/main/java/com/example/ettdemoproject/UI/LoginActivity.java
+++ app/src/main/java/com/example/ettdemoproject/UI/LoginActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.widget.Toolbar;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +20,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,13 +38,16 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "Login Status";
     private static final String SUCCESS_MSG = "signInWithEmail:success";
     private static final String EMAIL_FAILURE_MSG = "signInWithEmail:failure";
-    private static final String FAILURE_MSG = "Authentication failed";
     private static final String USERNAME_ERROR = "Enter Your Email/Username";
-    private static final String EMAIL_ERROR = "Enter Your Password";
-
+    private static final String PASSWORD_ERROR = "Enter Your Password";
+    private static final String PATH = "Users";
+    private static final String NO_USERNAME_ERROR = "User Doesn't exist";
+    private static final String NO_EMAIL_ERROR = "Email Doesn't exist";
+    private static final String WRONG_PASSWORD_ERROR = "Password isn't Correct";
 
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @BindView(R.id.loginToolBar)
     Toolbar loginToolbar;
@@ -70,15 +79,19 @@ public class LoginActivity extends AppCompatActivity {
                 username = usernameEditText.getText().toString();
                 password = passwordEditText.getText().toString();
 
-                if (TextUtils.isEmpty(username)) {
+                if (username.isEmpty()) {
                     usernameEditText.setError(USERNAME_ERROR);
                     return;
                 }
-                if (TextUtils.isEmpty(password)) {
-                    passwordEditText.setError(EMAIL_ERROR);
+                if (password.isEmpty()) {
+                    passwordEditText.setError(PASSWORD_ERROR);
                     return;
                 }
-                login(username, password);
+                if (username.contains("@")) {
+                    loginAuth(username, password);
+                } else {
+                    login();
+                }
             }
         });
 
@@ -91,6 +104,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
 
     @Override
     public void onStart() {
@@ -107,10 +121,11 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog.show();
     }
 
-    private void login(String username, String password) {
+    private void loginAuth(String username, String password) {
         showProgressDialog();
 
-        firebaseAuth.signInWithEmailAndPassword(username, password)
+        String email = username;
+        firebaseAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -120,13 +135,63 @@ public class LoginActivity extends AppCompatActivity {
                             progressDialog.dismiss();
                             startActivity(new Intent(LoginActivity.this, MainActivity.class));
                         } else {
-                            Log.w(TAG, EMAIL_FAILURE_MSG, task.getException());
-                            Toast.makeText(LoginActivity.this, FAILURE_MSG,
-                                    Toast.LENGTH_SHORT).show();
-                        }
+                            progressDialog.dismiss();
+                            firebaseAuth.fetchSignInMethodsForEmail(email).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                                    boolean checkEmail = !task.getResult().getSignInMethods().isEmpty();
+                                    if (!checkEmail) {
+                                        passwordEditText.setError(null);
+                                        usernameEditText.setError(NO_EMAIL_ERROR);
+                                        usernameEditText.requestFocus();
+                                    } else {
+                                        usernameEditText.setError(null);
+                                        passwordEditText.setError(WRONG_PASSWORD_ERROR);
+                                        passwordEditText.requestFocus();
+                                    }
+                                }
+                            });
 
-                        // ...
+                        }
                     }
                 });
+    }
+
+
+    private void login() {
+        showProgressDialog();
+
+        DatabaseReference reference = database.getReference(PATH);
+        Query checkUser = reference.orderByChild("username").equalTo(username);
+
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    //usernameEditText.setError(null);
+                    String passwordDB = dataSnapshot.child(username).child("password").getValue().toString();
+
+                    if (passwordDB.equals(password)) {
+                        progressDialog.dismiss();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    } else {
+                        progressDialog.dismiss();
+                        passwordEditText.setError(WRONG_PASSWORD_ERROR);
+                        passwordEditText.requestFocus();
+                    }
+                } else {
+                    progressDialog.dismiss();
+                    usernameEditText.setError(NO_USERNAME_ERROR);
+                    usernameEditText.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
